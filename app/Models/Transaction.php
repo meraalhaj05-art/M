c0e1373a69a342a8fb3ada4b760e9f45e4c11b56
meraalhaj05-art <meraalhaj05@gmail.com>
2026-03-23 18:34:41 +0300

    Delete app/Models/Transaction.php

--- app/Models/Transaction.php
+++
@@ -1,7 +0,0 @@
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
-class Transaction extends Model {
-    protected $fillable = ['user_id', 'type', 'amount', 'method', 'details', 'proof_image', 'status'];
-}
